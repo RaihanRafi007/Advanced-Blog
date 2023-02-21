de6753bb9946993838e08ecacc9f3784216f7339
RaihanRafi007 <rrafi0570@gamail.com>
2023-02-21 22:16:12 +0600

    settings updated

--- core/settings.py
+++ core/settings.py
@@ -10,8 +10,6 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DOMAIN=os.environ.get('DOMAIN')
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
